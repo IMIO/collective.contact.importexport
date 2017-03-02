804a3d3f4625e6883e10ea76111efc7ac8e19041
Benoît Suttor <bsuttor@imio.be>
2017-03-02 16:46:01 +0100

    Preparing release 0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.contact.importexport',
-    version='1.0a1',
+    version='0.1',
     description="An add-on for Plone for collective.contact suite",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

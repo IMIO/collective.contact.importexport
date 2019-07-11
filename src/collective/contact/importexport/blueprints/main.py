e08bb44aa568e5396253a195f84627d08133e04b
Stephan Geulette <s.geulette@imio.be>
2019-07-11 10:54:14 +0200

    Added specific logger with file handler

--- src/collective/contact/importexport/blueprints/main.py
+++ src/collective/contact/importexport/blueprints/main.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
 
+from collective.contact.importexport.utils import get_main_path
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from zope.interface import classProvides
 from zope.interface import implements
+
+import logging
+import os
+
+e_logger = logging.getLogger('ccie-transmo')
+
+
+class Initialization(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.workingpath = get_main_path(options.get('basepath', ''), options.get('subpath', ''))
+        lfh = logging.FileHandler(os.path.join(self.workingpath, 'ie.log'), mode='w')
+        lfh.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+        lfh.setLevel(logging.WARN)
+        e_logger.addHandler(lfh)
+
+    def __iter__(self):
+        for item in self.previous:
+            yield item
 
 
 class OrderOrganizations(object):

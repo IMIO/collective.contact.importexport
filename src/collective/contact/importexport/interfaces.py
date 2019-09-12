4783c04e9b4003116848d630378b48cbbd9ade60
Christophe Boulanger <christophe.boulanger@imio.be>
2019-09-12 13:49:42 +0200

    usage of the specialized layer

--- src/collective/contact/importexport/interfaces.py
+++ src/collective/contact/importexport/interfaces.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from collective.documentgenerator.interfaces import ICollectiveDocumentGeneratorLayer
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
-class ICollectiveContactImportexportLayer(IDefaultBrowserLayer):
+class ICollectiveContactImportexportLayer(ICollectiveDocumentGeneratorLayer):
     """Marker interface that defines a browser layer."""
 
 

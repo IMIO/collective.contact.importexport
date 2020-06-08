875e5750867b9f1d0f5f5684820a5eb6b57381e4
Stephan Geulette <s.geulette@imio.be>
2020-06-08 16:13:42 +0200

    Stay in ipdb section

--- src/collective/contact/importexport/blueprints/various.py
+++ src/collective/contact/importexport/blueprints/various.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from collective.contact.importexport.blueprints.main import ANNOTATION_KEY
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.utils import Condition
+from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
 
@@ -18,9 +20,12 @@ class BreakpointSection(object):
         condition = options['condition']
         self.condition = Condition(condition, transmogrifier, name, options)
         self.previous = previous
+        self.transmogrifier = transmogrifier
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
 
     def __iter__(self):
         for item in self.previous:
             if self.condition(item):
-                ipdb.set_trace(sys._getframe().f_back)  # Break!
+#                ipdb.set_trace(sys._getframe().f_back)  # Break!
+                ipdb.set_trace()  # Break!
             yield item

3a05457f4a57353b4bab83541e3f9e6376e2c014
Stephan Geulette <s.geulette@imio.be>
2021-02-16 14:36:17 +0100

    If commit, log in append into different files to keep real changes

--- src/collective/contact/importexport/blueprints/main.py
+++ src/collective/contact/importexport/blueprints/main.py
@@ -54,14 +54,19 @@ class Initialization(object):
         self.workingpath = get_main_path(safe_unicode(options.get('basepath', '')),
                                          safe_unicode(options.get('subpath', '')))
         self.portal = transmogrifier.context
-        lfh = logging.FileHandler(os.path.join(self.workingpath, 'ie_input_errors.log'), mode='w')
-        lfh.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
-        lfh.setLevel(logging.INFO)
-        e_logger.addHandler(lfh)
-        lfh = logging.FileHandler(os.path.join(self.workingpath, 'ie_shortlog.log'), mode='w')
-        lfh.setFormatter(logging.Formatter('%(message)s'))
-        lfh.setLevel(logging.INFO)
-        o_logger.addHandler(lfh)
+        pipe_commit = transmogrifier.context.REQUEST.get('_pipeline_commit_', False)
+        if pipe_commit:
+            efh = logging.FileHandler(os.path.join(self.workingpath, 'ie_input_errors_commit.log'), mode='a')
+            ofh = logging.FileHandler(os.path.join(self.workingpath, 'ie_shortlog_commit.log'), mode='a')
+        else:
+            efh = logging.FileHandler(os.path.join(self.workingpath, 'ie_input_errors.log'), mode='w')
+            ofh = logging.FileHandler(os.path.join(self.workingpath, 'ie_shortlog.log'), mode='w')
+        efh.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+        efh.setLevel(logging.INFO)
+        e_logger.addHandler(efh)
+        ofh.setFormatter(logging.Formatter('%(message)s'))
+        ofh.setLevel(logging.INFO)
+        o_logger.addHandler(ofh)
 
         # set global variables in annotation
         self.storage = IAnnotations(transmogrifier).setdefault(ANNOTATION_KEY, {})
